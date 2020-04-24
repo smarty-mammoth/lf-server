e332c20fa9e7c6bf1dd428b28c1ea57e7471634c
mammoth <smart-mammoth@mail.ru>
2020-04-24 13:25:57 +0400

    Dont worked

--- src/main/java/lazyfarm/server/services/GrowboxService.java
+++ src/main/java/lazyfarm/server/services/GrowboxService.java
@@ -1,32 +1,42 @@
 package lazyfarm.server.services;
 
+import lazyfarm.server.exeptions.APIException;
+import lazyfarm.server.response.CodeError;
 import lazyfarm.server.entities.Device;
 import lazyfarm.server.entities.GrowBox;
 import lazyfarm.server.entities.Sensor;
 import org.springframework.stereotype.Service;
+import lazyfarm.server.repositories.GrowboxRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GrowboxService {
+	
+	@Autowired
+	private GrowboxRepository growBoxRepo;
+	
     public List<GrowBox> getAllBoxes() {
-        return null;
+        return growBoxRepo.findAll();
     }
 
-    public GrowBox findById(Long id) {
-        return null;
+    public Optional<GrowBox> findById(Long id) {
+        return growBoxRepo.findById(id);
     }
 
-    public void addBox(String name) {
-
+    public void addBox(String name, String description) throws Exception {
+		growBoxRepo.save(new GrowBox(name, description));
     }
 
-    public GrowBox findByName(String name) {
-        return new GrowBox(name);
+    public Optional<GrowBox> findByName(String name) throws Exception {
+        return growBoxRepo.findByName(name);
     }
 
-    public void addDevice(String name, String address) {
-
+    public void addDevice(Long idGrowbox, String name, String address) throws Exception {
+		var growBox = growBoxRepo.findById(idGrowbox).orElseThrow(() -> new APIException(CodeError.GROWBOX_NOT_FOUND));
+		
     }
 
     public void addSensor(Long idGrowbox, Sensor sensor) {
